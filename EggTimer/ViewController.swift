a7527a075594ed1393754a973ecc128f7be78fa5
onaxiz <onaxiz@tutanota.com>
2023-05-08 21:22:16 +0300

    make application
    add progressbar
    add timer
    add timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
     
-
-
+    let eggTimes = [ "Soft" : 3, "Medium" : 420, "Hard" : 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        player?.stop()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let persentageProggress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = persentageProggress
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.mp3.rawValue)
+        player?.play()
+    }
 }
